8a1068e30536f45f1478af258d18293a0442969d
colorlessenergy <brianmunozdev@gmail.com>
2018-04-22 17:24:38 -0700

    update cart styles

--- _site/main.js
+++ _site/main.js
@@ -15,15 +15,26 @@ var shoppingbag = document.querySelector("#shopping-bag");
 
 shoppingbag.addEventListener("click", function () {
   cartshow.classList.toggle("menu-open");
+  $(".opaque__screen")[0].classList.toggle("open-opaque");
 });
 
 cartOpen.addEventListener("click", function () {
   cartshow.classList.toggle("menu-open");
+  $(".opaque__screen")[0].classList.toggle("open-opaque");
 });
+
+
 // snipcart
 
 var cartd = document.querySelector(".cart__content");
 
+// close the cart menu when pressing outside of it
+window.onclick = function (event) {
+  if (event.target == document.querySelector(".open-opaque")) {
+    cartshow.classList.toggle("menu-open");
+    $(".opaque__screen")[0].classList.toggle("open-opaque");
+  }
+}
 // the value of the input quantity update the value in the cart
 $('.desktop .buying__input').change(function () {
     $('.desktop .buying__button').data('item-quantity', $(this).val());
@@ -34,51 +45,74 @@ $('.mobile .buying__input').change(function () {
 });
 
 document.addEventListener('snipcart.ready', function() {
-    createItemCart(Snipcart.api.items.all(), cartd)
+	createItemCart(Snipcart.api.items.all(), cartd);
+	createCart(Snipcart.api.items.all(), $(".template-checkout-items"));
+
     Snipcart.subscribe('item.added', function (item) {
-      console.log(item, Snipcart.api.items.all());
+
       createItemCart(Snipcart.api.items.all(), cartd)
-      cartshow.classList.toggle("menu-open");
+			createCart(Snipcart.api.items.all(), $(".template-checkout-items"))
+			cartshow.classList.toggle("menu-open");
+      $(".opaque__screen")[0].classList.toggle("open-opaque");
     });
 });
 
 // create the meta data for each item in the quick cart.
 
 function createItemCart(obj, append) {
-  append.innerHTML = "";
+  if (append) {
+    append.innerHTML = "";
+		obj.forEach(function (current) {
+			$(append)
+				.append("<div class='cart__item'> <img src=" + current.image + " /> <div class='cart__meta'> <h2> " + current.description + " </h2> <p> quantity: " + current.quantity + " </p> <p> price: " + current.price + " </p> </div> <p class='exit'> x </p> </div>");
 
-  var allEle = [];
+			$(".exit").data("item-id", current.id);
 
-  obj.forEach(function (current) {
-    var createTitle = document.createElement("h2");
-    createTitle.innerHTML = current.description;
-    var createQuantity = document.createElement("p");
-    createQuantity.innerHTML = "quantity " + current.quantity;
-    var createPrice = document.createElement("p");
-    createPrice.innerHTML = "price " + current.price;
-    var createDelete = document.createElement("p");
-    createDelete.innerHTML = "x";
-    $(createDelete).data("item-id", current.id)
+			$(".exit").click(function () {
+				console.log($(this).data("item-id"), this)
+				Snipcart.api.items.remove($(this).data("item-id"))
+					.then(function (item) {
+						// remove the items both in the subcart and the actual cart page
+						createItemCart(Snipcart.api.items.all(), cartd);
+						createCart(Snipcart.api.items.all(), document.querySelector(".template-checkout-items"));
 
-    createDelete.addEventListener("click", function () {
-      Snipcart.api.items.remove($(this).data("item-id"))
-        .then(function (item) { 
-          // remove the items both in the subcart and the actual cart page
-          createItemCart(Snipcart.api.items.all(), cartd);
-          createItemCart(Snipcart.api.items.all(), document.querySelector(".checkout"))
-        });
-    })
+					});
+			});
 
-    allEle.push(createDelete);
-    allEle.push(createTitle);
-    allEle.push(createQuantity);
-    allEle.push(createPrice);
-  });
+		});
 
-  allEle.forEach(function (current){
-    append.appendChild(current);
-  });
+  }
 
+}
+
+function createCart(obj, append) {
+
+	if (append) {
+		
+		append.innerHTML = "";
+		obj.forEach(function (current) {
+		$(append)
+			.append(
+			'<div class= "template-checkout template-checkout-items"><div class="template-checkout__cart"><img src="' + current.image + '" alt=""><div class="cart__meta"><h2> ' + current.description + ' </h2><p> ID: ' + current.id + ' </p><p class="exit">remove</p></div></div><p class="template-checkout__price">$' + current.price + '</p><div class="template-checkout__qty"><input type="number" value="' + Number(current.quantity) + '" class="buying__input"></div><p class="template-checkout__total">$' + current.totalPrice + '</p></div>'
+			)
+
+			$(".exit").data("item-id", current.id);
+
+			$(".exit").click(function () {
+				console.log($(this).data("item-id"), this)
+				Snipcart.api.items.remove($(this).data("item-id"))
+					.then(function (item) {
+						// remove the items both in the subcart and the actual cart page
+						createItemCart(Snipcart.api.items.all(), cartd);
+
+						createCart(Snipcart.api.items.all(), document.querySelector(".template-checkout-items"));
+					});
+			});
+
+		});
+
+	};
+  
 }
 
 
