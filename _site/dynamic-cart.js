5a7a790c628590d4ab0c7aaa86da569230ae7a67
colorlessenergy <brianmunozdev@gmail.com>
2018-04-25 05:55:30 -0700

    update removing items actually removes the correct item

--- _site/dynamic-cart.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('snipcart.ready', function () {
-    createItemCart(Snipcart.api.items.all(), document.querySelector(".checkout"))
-});
--- main.js
+++ main.js
@@ -51,7 +51,9 @@ document.addEventListener('snipcart.ready', function() {
     Snipcart.subscribe('item.added', function (item) {
 
       createItemCart(Snipcart.api.items.all(), cartd)
-			createCart(Snipcart.api.items.all(), $(".template-checkout-items"))
+			createCart(Snipcart.api.items.all(), $(".template-checkout-items"));
+
+			// toggle css to focus on the cart
 			cartshow.classList.toggle("menu-open");
       $(".opaque__screen")[0].classList.toggle("open-opaque");
     });
@@ -61,23 +63,14 @@ document.addEventListener('snipcart.ready', function() {
 
 function createItemCart(obj, append) {
   if (append) {
-    append.innerHTML = "";
+		append.innerHTML = "";
 		obj.forEach(function (current) {
+			// console.log(current);
 			$(append)
-				.append("<div class='cart__item'> <img src=" + current.image + " /> <div class='cart__meta'> <h2> " + current.description + " </h2> <p> quantity: " + current.quantity + " </p> <p> price: " + current.price + " </p> </div> <p class='exit'> x </p> </div>");
+				.append("<div class='cart__item'> <img src=" + current.image + " /> <div class='cart__meta'> <h2> " + current.description + " </h2> <p> quantity: " + current.quantity + " </p> <p> price: " + current.price + " </p> </div> <p data-item-id='"+ current.id + "' class='exit'> x </p> </div>");
 
-			$(".exit").data("item-id", current.id);
+			deleteItem(current);
 
-			$(".exit").click(function () {
-				console.log($(this).data("item-id"), this)
-				Snipcart.api.items.remove($(this).data("item-id"))
-					.then(function (item) {
-						// remove the items both in the subcart and the actual cart page
-						createItemCart(Snipcart.api.items.all(), cartd);
-						createCart(Snipcart.api.items.all(), document.querySelector(".template-checkout-items"));
-
-					});
-			});
 
 		});
 
@@ -93,22 +86,10 @@ function createCart(obj, append) {
 		obj.forEach(function (current) {
 		$(append)
 			.append(
-			'<div class= "template-checkout template-checkout-items"><div class="template-checkout__cart"><img src="' + current.image + '" alt=""><div class="cart__meta"><h2> ' + current.description + ' </h2><p> ID: ' + current.id + ' </p><p class="exit">remove</p></div></div><p class="template-checkout__price">$' + current.price + '</p><div class="template-checkout__qty"><input type="number" value="' + Number(current.quantity) + '" class="buying__input"></div><p class="template-checkout__total">$' + current.totalPrice + '</p></div>'
+			'<div class= "template-checkout template-checkout-items"><div class="template-checkout__cart"><img src="' + current.image + '" alt=""><div class="cart__meta"><h2> ' + current.description + ' </h2><p> ID: ' + current.id + ' </p><p class="exit" data-item-id="'+ current.id + '">remove</p></div></div><p class="template-checkout__price">$' + current.price + '</p><div class="template-checkout__qty"><input type="number" value="' + Number(current.quantity) + '" class="buying__input"></div><p class="template-checkout__total">$' + current.totalPrice + '</p></div>'
 			)
 
-			$(".exit").data("item-id", current.id);
-
-			$(".exit").click(function () {
-				console.log($(this).data("item-id"), this)
-				Snipcart.api.items.remove($(this).data("item-id"))
-					.then(function (item) {
-						// remove the items both in the subcart and the actual cart page
-						createItemCart(Snipcart.api.items.all(), cartd);
-
-						createCart(Snipcart.api.items.all(), document.querySelector(".template-checkout-items"));
-					});
-			});
-
+			deleteItem(current);
 		});
 
 	};
@@ -116,3 +97,15 @@ function createCart(obj, append) {
 }
 
 
+function deleteItem(obj) {
+	$(".exit[data-item-id='" + obj.id + "']").click(function () {
+
+		Snipcart.api.items.remove($(this).data("item-id"))
+			.then(function (item) {
+				// remove the items both in the subcart and the actual cart page
+				createItemCart(Snipcart.api.items.all(), cartd);
+
+				createCart(Snipcart.api.items.all(), document.querySelector(".template-checkout-items"));
+			});
+	});
+}
